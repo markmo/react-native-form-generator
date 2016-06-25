bb42f31cad2c2bc9e2eaaffa15b2092ab4866376
Mark Moloney <mark.moloney@team.telstra.com>
2016-06-26 02:19:52 +1000

    add capability to clear form

--- src/Form.js
+++ src/Form.js
@@ -1,74 +1,75 @@
-import React from 'react';
-let { View, TextInput,
-  StyleSheet,
+import React, { Component } from 'react';
+import {
   ScrollView,
-  Text,
   SliderIOS,
-  TouchableWithoutFeedback
-} = require('react-native');
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 
-// import {Separator} from './fields/Separator';
+export class Form extends Component {
 
-export class Form extends React.Component{
   constructor(props){
-    super();
+    super(props);
     this.values = {};
-    React.Children.map(props.children, (child)=> {
+    React.Children.map(props.children, (child) => {
       if (!child) {
         return;
       }
-        if(child.ref){
-          this.values[child.ref] = child.props.value;
-        }
-
+      if (child.ref) {
+        this.values[child.ref] = child.props.value;
+      }
     });
   }
 
-  handleFieldFocused(event, inputHandle){
-      this.props.onFocus && this.props.onFocus(event, inputHandle);
+  handleFieldFocused(event, inputHandle) {
+    this.props.onFocus && this.props.onFocus(event, inputHandle);
   }
-  handleFieldChange(field_ref, value){
-     this.values[field_ref] = value;
+
+  handleFieldChange(field_ref, value) {
+    this.values[field_ref] = value;
     this.props.onChange && this.props.onChange(this.values);
   }
-  getValues(){
+
+  getValues() {
     return this.values;
   }
-  underscoreToSpaced(str){
-    var words = str.split('_');
-    var res=[];
-    words.map(function(word, i){
-      res.push(word.charAt(0).toUpperCase() + word.slice(1));
-    })
 
+  underscoreToSpaced(str) {
+    const words = str.split('_');
+    const res = [];
+    words.map((word, i) => {
+      res.push(word.charAt(0).toUpperCase() + word.slice(1));
+    });
     return res.join(' ');
   }
 
-  render(){
-    let wrappedChildren = [];
+  clear() {
+    for (const child of this.wrappedChildren) {
+      child.clear();
+    }
+  }
 
-    React.Children.map(this.props.children, (child, i)=> {
+  render() {
+    const wrappedChildren = [];
+    React.Children.map(this.props.children, (child, i) => {
       if (!child) {
         return;
       }
-
-      //if (child.type === this){
-        wrappedChildren.push(React.cloneElement(child, {
-          key: child.ref || child.type+i,
-          fieldRef : child.ref,
-          onFocus:this.handleFieldFocused.bind(this),
-          onChange:this.handleFieldChange.bind(this)
-        }
-      ));
-      //}
-    }, this)
-
+      wrappedChildren.push(React.cloneElement(child, {
+        key: child.ref || child.type + i,
+        fieldRef: child.ref,
+        onFocus: this.handleFieldFocused.bind(this),
+        onChange: this.handleFieldChange.bind(this),
+      }));
+    }, this);
+    this.wrappedChildren = wrappedChildren;
     return (
       <View style={this.props.style}>
-          {wrappedChildren}
+        {wrappedChildren}
       </View>
-
-
-      );
+    );
   }
 }
--- src/lib/InputComponent.js
+++ src/lib/InputComponent.js
@@ -1,29 +1,29 @@
 'use strict';
 
-import React from 'react';
-import ReactNative from 'react-native';
-import {Field} from './Field.js';
+import React, { Component } from 'react';
+import { View, StyleSheet, TextInput, Text } from 'react-native';
+import { Field } from './Field.js';
 
-let { View, StyleSheet, TextInput, Text} = ReactNative;
 
 function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
 
-export class InputComponent extends React.Component{
+export class InputComponent extends Component {
+
   constructor(props){
-    super();
+    super(props);
     this.state = {
       labelWidth: 0,
       value: props.value,
       minFieldHeight: props.height || 44,
       inputHeight: Math.max(props.height || 44)
     }
-    if(props.validationFunction) {
+    if (props.validationFunction) {
       this.valid = props.validationFunction(value, this);
-    } else{
-      if(props.keyboardType){
+    } else {
+      if (props.keyboardType) {
         switch (props.keyboardType) {
           case 'email-address':
             this.valid = validateEmail(props.value);
@@ -31,77 +31,70 @@ export class InputComponent extends React.Component{
         }
       }
     }
-
   }
-  handleLayoutChange(e){
-    let {x, y, width, height} = {... e.nativeEvent.layout};
 
+  handleLayoutChange(e){
+    const {x, y, width, height} = {... e.nativeEvent.layout};
     this.setState(e.nativeEvent.layout);
-    //e.nativeEvent.layout: {x, y, width, height}}}.
   }
 
   handleLabelLayoutChange(e){
-    let {x, y, width, height} = {... e.nativeEvent.layout};
-
+    const {x, y, width, height} = {... e.nativeEvent.layout};
     this.setState({labelWidth:width});
-    //e.nativeEvent.layout: {x, y, width, height}}}.
   }
+
   handleChange(event){
-
-    var value = event.nativeEvent.text;
-
-    if(this.props.validationFunction) {
+    const value = event.nativeEvent.text;
+    if (this.props.validationFunction) {
       this.valid = this.props.validationFunction(value, this);
-    } else
-    if(this.props.keyboardType){
+    } else if (this.props.keyboardType) {
       switch (this.props.keyboardType) {
         case 'email-address':
           this.valid = validateEmail(value);
           break;
       }
     }
-    this.setState({value:value,
-      inputHeight: Math.max(this.state.minFieldHeight,
-        (event.nativeEvent.contentSize && this.props.multiline)?event.nativeEvent.contentSize.height:0)
+    this.setState({
+      value,
+      inputHeight: Math.max(
+        this.state.minFieldHeight,
+        (event.nativeEvent.contentSize && this.props.multiline) ? event.nativeEvent.contentSize.height : 0
+      )
       });
-    //this.props.onChange(this.props.fieldRef, value);
-    if(this.props.onChange)      this.props.onChange(this.props.fieldRef, value);
-    if(this.props.onValueChange) this.props.onValueChange(value);
+    if (this.props.onChange) {
+      this.props.onChange(this.props.fieldRef, value);
+    }
+    if (this.props.onValueChange) {
+      this.props.onValueChange(value);
+    }
   }
 
   _scrollToInput (event) {
-    //debugger;
     if (this.props.onFocus) {
-      let handle = ReactNative.findNodeHandle(this.refs.inputBox);
-      this.props.onFocus(
-        event,
-        handle
-      )
+      const handle = ReactNative.findNodeHandle(this.refs.inputBox);
+      this.props.onFocus(event, handle);
     }
   }
-  handleFieldPress(event){
+
+  handleFieldPress(event) {
     this.refs.inputBox.focus();
   }
-  render(){
- // style={[formStyles.fieldContainer,
- //     formStyles.horizontalContainer,
- //     this.props.containerStyle,
- //     {height: this.state.inputHeight+1}
- //   ]}
 
-    return(<Field {...this.props}>
+  clear() {
+    this.refs.inputBox.clear();
+  }
+
+  render() {
+    return (
+      <Field {...this.props}>
         <View
           onLayout={this.handleLayoutChange.bind(this)}
           style={[
               this.props.containerStyle,
-              {height: this.state.inputHeight+1}
+              { height: this.state.inputHeight + 1 }
             ]}>
-          {(this.props.iconLeft)
-            ? this.props.iconLeft
-            : null
-          }
-          {(this.props.label)
-            ?
+          {this.props.iconLeft ? this.props.iconLeft : null}
+          {this.props.label ?
             <Text style={this.props.labelStyle}
               onLayout={this.handleLabelLayoutChange.bind(this)}
               onPress={this.handleFieldPress.bind(this)}
@@ -110,41 +103,32 @@ export class InputComponent extends React.Component{
           }
           <TextInput
             {...this.props}
-            ref='inputBox'
-            keyboardType = {this.props.keyboardType}
+            ref="inputBox"
+            keyboardType={this.props.keyboardType}
             style={[
                 this.props.inputStyle,
                 {height: this.state.inputHeight}
               ]}
-
             onChange={this.handleChange.bind(this)}
             onFocus={this._scrollToInput.bind(this)}
             placeholder={this.props.placeholder}
             value={this.state.value}
-            width={this.state.width-this.state.labelWidth
-                -((this.props.iconRight)?this.props.iconRight.props.size:0)
-                -((this.props.iconLeft)?this.props.iconLeft.props.size:0)
+            width={
+              this.state.width
+              - this.state.labelWidth
+              - (this.props.iconRight ? this.props.iconRight.props.size : 0)
+              - (this.props.iconLeft ? this.props.iconLeft.props.size : 0)
               }
-
             />
-          {(this.props.iconRight)
-              ? this.props.iconRight
-              : null
-            }
+          {this.props.iconRight ? this.props.iconRight : null}
         </View>
-    </Field>
-  )
+      </Field>
+    );
+  }
 }
-
-}
-
-// InputComponent.propTypes = {
-//   multiline: React.PropTypes.bool,
-//   placeholder:React.PropTypes.string,
-// }
 
 InputComponent.propTypes = {
   labelStyle: Text.propTypes.style,
   inputStyle: TextInput.propTypes.style,
-  containerStyle: View.propTypes.style
+  containerStyle: View.propTypes.style,
 }
